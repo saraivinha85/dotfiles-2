c496d98d031e4627e577b129cb56f303f93d969e
Sogomn <sogomn@Sogomn.fritz.box>
2018-03-17 14:36:00 +0100

    Update

--- update.sh
+++ update.sh
@@ -1,12 +1,16 @@
 rm -r .config
 rm -r Scripts
+
 mkdir .config
+
 cp ~/.bashrc .
 cp ~/.Xmodmap .
 cp ~/.config/compton.conf .config/
+
 cp -r ~/.config/feh .config/
 cp -r ~/.config/dunst .config/
 cp -r ~/.config/i3 .config/
 cp -r ~/.config/i3blocks .config/
 cp -r ~/.config/alacritty .config/
+
 cp -r ~/Scripts .
