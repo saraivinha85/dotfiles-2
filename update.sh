aafbccf2fc5e54a236bf3f5e28834d4656417ff4
Johannes Boczek <johannes.boczek@web.de>
2018-03-18 14:36:42 +0100

    Update

--- update.sh
+++ update.sh
@@ -1,13 +1,18 @@
 rm -r .config
 rm -r Scripts
+rm -r .atom
 
 mkdir .config
+mkdir .atom
 
 cp ~/.bashrc .
 cp ~/.bash_profile .
 cp ~/.Xmodmap .
 cp ~/.xinitrc .
 cp ~/.config/compton.conf .config/
+cp ~/.atom/config.cson .atom/
+cp ~/.atom/keymap.cson .atom/
+cp ~/.atom/styles.less .atom/
 
 cp -r ~/.config/feh .config/
 cp -r ~/.config/dunst .config/
