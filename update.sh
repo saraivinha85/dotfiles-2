6c0d1c75ca0a2cbb7e99dc3833c169f2a8706283
Johannes Boczek <johannes.boczek@web.de>
2018-04-09 00:47:57 +0200

    Update

--- update.sh
+++ update.sh
@@ -19,5 +19,6 @@ cp -r ~/.config/dunst .config/
 cp -r ~/.config/i3 .config/
 cp -r ~/.config/i3blocks .config/
 cp -r ~/.config/alacritty .config/
+cp -r ~/.config/rofi .config/
 
 cp -r ~/Scripts .
