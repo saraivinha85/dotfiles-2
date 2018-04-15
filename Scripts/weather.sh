9a96526cf9a647136093d5cda21073efa255e292
Johannes Boczek <johannes.boczek@web.de>
2018-04-16 00:09:17 +0200

    Update

--- Scripts/weather.sh
+++ Scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WEATHER=$(curl -sNm 0.5 "wttr.in/Hamburg" | head -n 7 | tail -n 5)
+WEATHER=$(curl --silent -N -m 0.5 "wttr.in/Hamburg" | head -n 7 | tail -n 5)
 
 if [ ! -z "$WEATHER" ]
 then
